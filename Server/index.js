4d9db6514c111827d3a92b45dc8cb610ac233957
arindam saha <112413589+arindamsaha2022@users.noreply.github.com>
2024-01-29 09:48:21 -0500

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -12,7 +12,13 @@ app.use(express.json({limit: "30mb", extended: true}))
 app.use(express.urlencoded({limit: "30mb", extended: true}))
 app.use(cors());
 const PORT =process.env.PORT || 5000
-
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+))
 app.get('/',(req, res) => {
     res.send("This is a stack overflow clone API")
 })
